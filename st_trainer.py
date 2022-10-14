4c3c78136b1a989b968ffa55d94e918aa69ffa40
paolorechia <paolorechia@gmail.com>
2022-10-14 18:00:54 +0200

    Multi task trainer :)

--- st_trainer.py
+++ st_trainer.py
@@ -52,7 +52,62 @@ def auto_trainer(con: TrainingContext):
 
 
 def train_model_on_all_attributes(con: TrainingContext):
-    raise NotImplementedError("Oh no")
+    output_path = os.path.join(
+        con.output_dir,
+        f"{con.model_info.model_name}-multitask-{str(con.unique_id[0:8])}",
+    )
+    train_df, test_df = create_attribute_stratified_split(
+        con.attribute, con.test_size, dataset=con.input_dataset
+    )
+
+    small_subset = sample_sentences_per_class(
+        train_df, con.attribute, con.max_samples_per_class
+    )
+
+    train_objectives = []
+
+    for attr in con.attributes:
+        training_dataset: TrainingDataset = create_continuous_sentence_pairs(
+            small_subset,
+            con.text_label,
+            attr,
+            con.model_info.model_truncate_length,
+            "training",
+        )
+        # Define your train dataset, the dataloader and the train loss
+
+        train_dataloader = DataLoader(
+            training_dataset.training_pairs, shuffle=True, batch_size=con.batch_size
+        )
+        train_loss = losses.CosineSimilarityLoss(con.model)
+
+        train_objectives.append((train_dataloader, train_loss))
+
+    def evaluation_callback(score, epoch, steps):
+        print(f"\n\n\tEpoch {epoch} - Evaluation score: {score} - Steps: {steps}\n\n")
+
+    print("Starting training, results will be saved to: ", output_path)
+    # Tune the model
+    con.model.fit(
+        train_objectives=train_objectives,
+        epochs=con.num_epochs,
+        # evaluator=evaluator # Not sure if this is supported in multi-task,
+        warmup_steps=con.warmup_steps,
+        weight_decay=con.weight_decay,
+        output_path=output_path,
+        save_best_model=True,
+        steps_per_epoch=con.train_steps,
+        optimizer_params={"lr": con.learning_rate},
+        show_progress_bar=True,
+        callback=evaluation_callback,
+        checkpoint_save_steps=con.checkpoint_steps,
+        checkpoint_path=os.path.join(
+            con.checkout_dir,
+            con.model_info.model_name,
+            con.attribute,
+            str(con.unique_id[0:8]),
+        ),
+    )
 
 
 def train_model_on_single_attribute(con: TrainingContext):
