81a220f99c659589ee9fa0207e6355c73ff02d55
Paolo Rechia <paolorechia@gmail.com>
2022-10-16 06:58:51 +0200

    Add imports to seeds.py

--- seeds.py
+++ seeds.py
@@ -2,3 +2,11 @@ PANDAS_RANDOM_STATE = 10
 PYTORCH_SEED = 12
 RANDOM_SEED = 22
 NUMPY_SEED = 42
+
+import torch
+import random
+import numpy as np
+
+torch.manual_seed(PYTORCH_SEED)
+random.seed(RANDOM_SEED)
+np.random.seed(NUMPY_SEED)
