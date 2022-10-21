ebb4bcd15f3b218496da6f60790d95d7900987cf
paolorechia <paolorechia@gmail.com>
2022-10-21 12:20:17 +0200

    Fix baseline

--- simple_baseline/baseline.py
+++ simple_baseline/baseline.py
@@ -1,15 +1,40 @@
 """
-Baseline without balancing the data achieves a score of 0.63 (MCRMSE).
+Baseline without balancing the data, RandomForestClassifier achieves a score of 0.63 (MCRMSE) (0.65 on Fold).
 Applying class weights to the random forest model decreaes the score to 1.41 (lower is better) 
+With RidgeClassifierCV, we can get a score of 0.62 on Fold
+
+If we instead model it as a regression problem, we can get a score of 0.54 on Fold with RidgeCV
+If we apply round_border_score function, makes no absolute difference.
+
 """
 
 import os
 
 import pandas as pd
+from sklearn.linear_model import RidgeClassifierCV, RidgeCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import KFold
 from sklearn.pipeline import Pipeline
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+
+def calculate_rmse_score(y_true, y_pred):
+    rmse_scores = []
+    for i in range(len(attributes)):
+        rmse_scores.append(np.sqrt(mean_squared_error(y_true[:, i], y_pred[:, i])))
+    return np.mean(rmse_scores)
+
+
+def round_border_score(float_score: float):
+    if float_score < 1.0:
+        return 1.0
+    if float_score > 5.0:
+        return 5.0
+    return float_score
+
 
 output_dir = "./"
 data_dir = "/data/feedback-prize/"
@@ -47,16 +72,16 @@ labels = {
 }
 reverse_labels = {v: float(k) for k, v in labels.items()}
 
-for attr in attributes:
-    df_train[f"{attr}_label"] = df_train.apply(
-        lambda x: labels[str(getattr(x, attr))], axis=1
-    )
-    print(
-        "Attribute: ",
-        attr,
-        " unique values: ",
-        df_train[f"{attr}_label"].value_counts(),
-    )
+# for attr in attributes:
+#     df_train[f"{attr}_label"] = df_train.apply(
+#         lambda x: labels[str(getattr(x, attr))], axis=1
+#     )
+#     print(
+#         "Attribute: ",
+#         attr,
+#         " unique values: ",
+#         df_train[f"{attr}_label"].value_counts(),
+#     )
 
 X = df_train.full_text
 n_splits = 5
@@ -78,11 +103,13 @@ for train_index, test_index in kfold.split(X):
     X_train = X.filter(items=train_index, axis=0)
     X_test = X.filter(items=test_index, axis=0)
 
+    predictions_df = pd.DataFrame()
     # Train one classifier for each attribute
     label_dfs = {}
     for attribute in attributes:
         label_dfs[attribute] = {}
-        y = df_train[f"{attribute}_label"]
+        y = df_train[attribute]
+        # y = df_train[f"{attribute}_label"]
 
         label_dfs[attribute]["train"] = y.filter(items=train_index, axis=0)
         label_dfs[attribute]["test"] = y.filter(items=test_index, axis=0)
@@ -99,38 +126,25 @@ for train_index, test_index in kfold.split(X):
             [
                 ("vect", CountVectorizer()),
                 ("tdidf", TfidfTransformer()),
-                (
-                    "clf",
-                    RandomForestClassifier(
-                        max_depth=5,
-                        n_estimators=20,
-                        max_features=1,
-                        random_state=42,
-                        class_weight=weights,
-                    ),
-                ),
+                # ("clf", RidgeClassifierCV()),
+                ("clf", RidgeCV()),
             ]
         )
         pipeline.fit(X_train, y_train)
 
-    idx = test_index[0]
-    for x_ in X_test:
-        column_wise_sum = 0
-        for attribute in attributes:
-            y_test_ = label_dfs[attribute]["test"]
-            y_ = y_test_[idx]
+        # predictions_df[attribute] = [
+        #     reverse_labels[l] for l in pipeline.predict(X_test)
+        # ]
+        predictions_df[attribute] = [
+            round_border_score(p) for p in pipeline.predict(X_test)
+        ]
 
-            column_wise_sum += (
-                reverse_labels[pipeline.predict([x_])[0]] - reverse_labels[y_]
-            ) ** 2
-        column_wise_sum = column_wise_sum / len(attributes)
+    test_df = df_train.filter(items=test_index, axis=0)
+    fold_MCRMSE = calculate_rmse_score(
+        predictions_df[attributes].values, test_df[attributes].values
+    )
 
-        fold_MCRMSE += column_wise_sum
-        idx += 1
-
-    fold_MCRMSE = fold_MCRMSE / len(X_test)
-
-    print("fold_MCRMSE: ", fold_MCRMSE)
+    print("MCRMSE: ", fold_MCRMSE)
 
     best_classifier = trained_classifiers[attribute]
     best_error = best_fold[attribute]
@@ -144,13 +158,13 @@ for train_index, test_index in kfold.split(X):
 print(f"Best training MCRMSE: ", best_fold[attributes[0]])
 
 # Predict on challenge set
-challenge_X = df_challenge.full_text
-print(challenge_X.head())
-for attribute in attributes:
-    predictions = trained_classifiers[attribute].predict(challenge_X)
-    df_challenge[attribute] = [reverse_labels[pred] for pred in predictions]
+# challenge_X = df_challenge.full_text
+# print(challenge_X.head())
+# for attribute in attributes:
+#     predictions = trained_classifiers[attribute].predict(challenge_X)
+#     df_challenge[attribute] = [reverse_labels[pred] for pred in predictions]
 
-df_challenge.drop(columns=["full_text"], inplace=True)
-print(df_challenge.head())
-output_path = os.path.join(output_dir, "submission.csv")
-df_challenge.to_csv(output_path, index=False, float_format="%.1f")
+# df_challenge.drop(columns=["full_text"], inplace=True)
+# print(df_challenge.head())
+# output_path = os.path.join(output_dir, "submission.csv")
+# df_challenge.to_csv(output_path, index=False, float_format="%.1f")
